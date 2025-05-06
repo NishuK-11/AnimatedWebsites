61b17c2d00b17ab1d61d713fd186d825126c3cef
NishuK-11 <nishu.240103211@iiitbh.ac.in>
2025-05-06 23:48:28 +0530

    new files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+import React from 'react';
+import './index.css';
+import Navbar from './components/Navbar.jsx';
+import Banner from './components/Banner.jsx';
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold text-blue-500">
+    </div>
+    <Navbar />  
+    <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+import daisyui from 'daisyui';
+
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
+}
