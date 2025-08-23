dad1949d978ff80b3fc64bf907bb44ea0d04c9d2
NishuK-11 <nishu.240103211@iiitbh.ac.in>
2025-08-23 21:45:22 +0530

    gspa project

--- deployedProject/script.js
+++ deployedProject/script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function videoAnimation(){
     const play = document.getElementById('play');
     const videoContainer = document.getElementById('video-container');
@@ -19,14 +24,46 @@ function videoAnimation(){
     });
     videoContainer.addEventListener('mousemove', function(dets) {
         gsap.to(play,{
-            left:dets.x-30,
-            top:dets.y-30
+            left:dets.x-10,
+            top:dets.y-70
         })
 
     });
 }
 videoAnimation()
 
-gsap.from('#page1 h1',{
-    
+function loadingAnimation(){
+    gsap.from('#page1 h1',{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.2
+    })
+    gsap.from('#page1 #video-container',{
+    scale:0.9,
+    opacity:0,
+    delay:1.3,
+    duration:0.3,
+    stagger:0.2
+    })
+}
+loadingAnimation();
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+document.querySelector("#child1").addEventListener("mouseenter",function(){
+    gsap.to("#cursor",{
+        transform:'translate(-50%,-50%) scale(1)'
+    })
+})
+document.querySelector("#child1").addEventListener("mouseleave",function(){
+    gsap.to("#cursor",{
+        transform:'translate(-50%,-50%) scale(0)'
+    })
 })
